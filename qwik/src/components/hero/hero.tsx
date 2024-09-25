1f5e9f11f5d1351dbed725528a289836496beb98
monica-2707 <monicasrinivas6@gmail.com>
2024-09-26 02:14:51 +0530

    4946

--- qwik/src/components/hero/hero.tsx
+++ qwik/src/components/hero/hero.tsx
@@ -8,13 +8,7 @@ export default component$(() => {
       <ImgThunder class={styles["hero-image"]} alt="Image thunder" />
       <div class="container container-center container-spacing-xl">
         <h2>
-          Connect with <span class="highlight">F</span>
-          <span class="high1">L</span>
-          <span class="high2">A</span>
-          <span class="high3">V</span>
-          <span class="high4">O</span>
-          <span class="high5">U</span>
-          <span class="high6">R</span>
+          Connect with <span class="highlight">Flavour</span>
         </h2>
       </div>
     </div>
