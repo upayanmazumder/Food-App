451f5b915a5f4e358ba685c94543ca6659e42e72
upayanmazumder <upayanm3@gmail.com>
2024-09-25 17:41:39 +0530

    Refactor signedout, notsignedin, and signedin route components

--- qwik/src/routes/plugin@auth.ts
+++ qwik/src/routes/plugin@auth.ts
@@ -2,12 +2,15 @@ import { QwikAuth$ } from "@auth/qwik";
 import Google from "@auth/qwik/providers/google";
 
 export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
-  () => ({
-    providers: [
-      Google({
-        clientId: process.env.AUTH_GOOGLE_ID,
-        clientSecret: process.env.AUTH_GOOGLE_SECRET,
-      }),
-    ],
-  }),
+  () => {
+
+    return {
+      providers: [Google],
+      pages: {
+        signIn: "/a/signin/",
+        signOut: "/a/signout/",
+        signedOut: "/a/signedout/"
+      }
+    };
+  },
 );
