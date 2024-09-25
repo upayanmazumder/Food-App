f9d8f800ccbac9173ab7ee29ff59e37f80b7c6b5
upayanmazumder <upayanm3@gmail.com>
2024-09-25 17:28:14 +0530

    Refactor hero component to update the app title and remove unnecessary code

--- qwik/src/routes/index.tsx
+++ qwik/src/routes/index.tsx
@@ -1,18 +1,18 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+import Hero from "../components/hero/hero";
+
 export default component$(() => {
   return (
     <>
-
+      
+      <Hero />
       <div role="presentation" class="ellipsis"></div>
       <div role="presentation" class="ellipsis ellipsis-purple"></div>
 
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          A <span class="highlight">healthy</span> food app
-        </h3>
-      </div>
+
+
     </>
   );
 });
