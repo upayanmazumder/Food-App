0d3c00bcdd42016ae87577b9882d03cf6ec82d9d
upayanmazumder <upayanm3@gmail.com>
2024-09-25 15:31:35 +0530

    les go

--- qwik/src/routes/plugin@auth.ts
+++ qwik/src/routes/plugin@auth.ts
@@ -1,36 +1,13 @@
-import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { QwikAuth$ } from "@auth/qwik";
 import Google from "@auth/qwik/providers/google";
-import { cert, initializeApp, getApps } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
-
-const firebaseAdminInit = () => {
-  if (!getApps().length) {
-    initializeApp({
-      credential: cert({
-        projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
-        clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') || '',
-      }),
-    });
-  }
-};
 
 export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
-  () => {
-    firebaseAdminInit();
-    const firestore = getFirestore();
-
-    return {
-      providers: [Google],
-      adapter: FirestoreAdapter({
-        firestore,
+  () => ({
+    providers: [
+      Google({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       }),
-      pages: {
-        signIn: "/a/signin/",
-        signOut: "/a/signout/",
-        signedOut: "/a/signedout/"
-      }
-    };
-  },
+    ],
+  }),
 );
