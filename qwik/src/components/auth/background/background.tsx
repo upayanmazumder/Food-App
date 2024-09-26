29b4e619c98a4fbdef77823ad00c4d1f184a9538
upayanmazumder <upayanm3@gmail.com>
2024-09-26 12:14:34 +0530

    Refactor createPost route to remove unnecessary h2 element and update styling

--- qwik/src/components/auth/background/background.tsx
+++ qwik/src/components/auth/background/background.tsx
@@ -7,9 +7,6 @@ export default component$(() => {
     <div class={["container", styles.hero]}>
       <ImgThunder class={styles["hero-image"]} alt="Image thunder" />
       <div class="container container-center container-spacing-xl">
-        <h2>
-          Connect with <span class="highlight">Flavour</span>
-        </h2>
       </div>
     </div>
   );
--- qwik/src/routes/a/layout.tsx
+++ qwik/src/routes/a/layout.tsx
@@ -29,8 +29,8 @@ export default component$(() => {
     <>
 
       <main>
-        <AuthBackground />
         <Slot />
+        <AuthBackground />
       </main>
     </>
   );
