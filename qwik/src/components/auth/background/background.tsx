caabd78232343259f28598d9e9f2273c9dd0a81a
upayanmazumder <upayanm3@gmail.com>
2024-09-26 07:41:00 +0530

    ggg

---
+++ qwik/src/components/auth/background/background.tsx
@@ -0,0 +1,16 @@
+import { component$ } from "@builder.io/qwik";
+import styles from "./background.module.css";
+import ImgThunder from "../../../media/authentication/background.jpg?jsx";
+
+export default component$(() => {
+  return (
+    <div class={["container", styles.hero]}>
+      <ImgThunder class={styles["hero-image"]} alt="Image thunder" />
+      <div class="container container-center container-spacing-xl">
+        <h2>
+          Connect with <span class="highlight">Flavour</span>
+        </h2>
+      </div>
+    </div>
+  );
+});
--- qwik/src/routes/a/signedout/index.tsx
+++ qwik/src/routes/a/signedout/index.tsx
@@ -1,11 +1,9 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import AuthBg from "../../../media/authentication/background.jpg?jsx"
-import styles from "../auth.module.css"
+
 export default component$(() => {
   return (
     <>
-    <AuthBg class={styles["hero-image"]} alt="Image thunder" />
       <div class="container container-center container-spacing-xl">
         <h3>Signed out!</h3>
         <p>You are now signed out!</p>
