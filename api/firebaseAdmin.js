9fba1fd2218172171416ea0f66d7084e61b6357a
upayanmazumder <upayanm3@gmail.com>
2024-09-26 10:43:25 +0530

    gg

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -5,9 +5,10 @@ const path = require('path');
 // Load Firebase configuration from the JSON file
 const firebaseConfig = JSON.parse(fs.readFileSync(path.join(__dirname, 'firebaseconfig.json'), 'utf8'));
 
-// Initialize the Firebase Admin SDK
+// Initialize the Firebase Admin SDK with the storage bucket
 admin.initializeApp({
   credential: admin.credential.cert(firebaseConfig),
+  storageBucket: 'food-app-6c3d4.appspot.com', // Replace with your actual bucket name
 });
 
 module.exports = admin;
